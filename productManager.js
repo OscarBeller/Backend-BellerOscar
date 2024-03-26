70ef4fb9ececce3ca9d9d75d9b79ef82435633f2
Oscar Beller <gabrieloscarbeller@hotmail.com>
2024-03-26 19:55:16 -0300

    Empezando el tercer desafio

---
+++ productManager.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+
+
+class ProductManager {
+    #products;
+    #path;
+    static idProducto = 0;
+
+    constructor() {
+        this.#path = "./data/productos.json";
+        this.#products = this.#leerProdcutosInfile();
+    }
+
+    #asignarIdProducto(){
+        let id = 1; 
+        if (this.#products.length != 0)
+            id = this.#products[this.#products.length - 1].id + 1;
+        return id;
+    }
+
+    #leerProdcutosInfile(){
+        try {
+            if(fs.existsSync(this.#path)){
+                return JSON.parse(fs.readFileSync(this.#path, 'utf-8'));
+            }
+            return [];
+        } catch (error) {
+            console.log(`ocurrio un error al momento de leer el archivo de productos, ${error}`);
+            
+        }
+    }
+
+    #guardarArchivo(){
+        try {
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products));
+        } catch (error) {
+            console.log(`ocurrio un error al momento de guardar el archivo de productos, ${error}`);
+        }
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock)
+            return 'Todos los campos son obligatorios';
+
+        const codeRepeat = this.#products.some(product => product.code === code);
+        if (codeRepeat)
+            return `Error, el código ${code} se está repitiendo`;
+
+        ProductManager.idProducto = ProductManager.idProducto + 1;
+        const id = this.#asignarIdProducto();
+
+        const newProduct = {
+            id,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        };
+
+        this.#products.push(newProduct);
+        this.#guardarArchivo()
+        return 'El producto se ha añadido correctamente';
+    }
+
+    getProducts() {
+        return this.#products;
+    }
+
+    getProductsById(id) {
+        const producto = this.#products.find(product => product.id === id);
+        if (producto) {
+            return producto;
+        } else {
+            return `No se encontraron productos con la ID: ${id}`
+        };
+    }
+
+    updateProduct(id, objetUpdate){
+        let msg  = `El producto con id ${id} no existe`;
+
+        const index = this.#products.findIndex(p => p.id === id);
+        if (index !== -1){
+            const{id, ...rest} = objetUpdate;
+            this.#products[index] = {...this.#products[index], ...rest};
+            this.#guardarArchivo();
+            msg= 'Producto actualizado';
+        } 
+        return  "Se ha actualizado el producto";
+    }
+
+    deleteProduct(id){
+        let msg = `El producto con id  ${id } no existe`
+        const index = this.#products.findIndex(product => product.id === id);
+        if (index !== -1) {
+            this.#products = this.#products.filter(product => product.id !== id);
+            this.#guardarArchivo()
+            msg = "Se eliminó el producto correctamente";
+    }
+    return msg;
+}
+}
+
+module.exports = ProductManager;
